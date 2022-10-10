3ddc913d385a6506f4d5b6367fa2fac4d368835b
valentinfrlch <valentin.froehlich@gmail.com>
2022-10-10 18:48:07 +0200

    added mood

--- deep.py
+++ deep.py
@@ -1,32 +1,58 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import json
 
 # convert csv to dataframe
+
 
 def synthesize(df, value):
     if value == "sleep_delta":
         v = df["Sleep Analysis [Asleep] (hr)"] - \
             df["Sleep Analysis [In Bed] (hr)"]
     elif value == "mood":
-        v = add_mood(df)
+        v = mood(df)
     else:
         v = None
     return v
 
 
+def mood(df):
+    # add json to dataframe
+    with open('dataset/mood.json') as f:
+        data = json.load(f)
+
+        date = []
+        happiness = []
+        tags = []
+
+        for i in data:
+            date.append(i['date'])
+            happiness.append(i['mood'])
+            tags.append(i['emotions'])
+
+        for d, h, t in zip(date, happiness, tags):
+            # locate row with matching date
+            date = df.loc[df['Date'] == d]
+            # get index of row
+            i = date.index[0]
+            # insert happiness and tags into dataframe at index
+            df.at[i, 'Mood'] = h
+            # df.at[i, 'Tags'] = t
+
 
 def convert(csv):
     # remove all empty columns from csv
     df = pd.read_csv(csv)
+    # convert date column to datetime
+    df.Date = pd.to_datetime(df.Date)
     df.dropna(axis=1, how='all', inplace=True)
     df.insert(47, "Sleep Delta (hr)", synthesize(df, "sleep_delta"))
-    # df.insert(48, "Mood", synthesize(df, "mood"))
+    mood(df)
     print("converted")
     return df
-    
-    
+
+
 def correlation(df):
     # correlation matrix
     corr = df.corr()
@@ -39,7 +65,7 @@ def correlation(df):
     # plt.show()
     # save the figure to file
     plt.savefig('visualisations/correlation.png')
-    
+
 
 def pair(df):
     g = sns.pairplot(df[['Sleep Delta (hr)', 'Blood Pressure [Systolic] (mmHg)']],
@@ -51,12 +77,10 @@ def pair(df):
     g.map_offdiag(sns.scatterplot, marker='.', color='black')
     g.fig.set_size_inches(50, 50)
     plt.show()
-    
-    
+
+
 if __name__ == '__main__':
     df = convert('dataset/export.csv')
     correlation(df)
-    #pair(df)
-    
-    
-    
+    # pair(df)
+    # mood(df)
