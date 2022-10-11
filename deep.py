4563e7ba7d4b0d7d8f0df5c69e14fb927eb57f70
valentinfrlch <valentin.froehlich@gmail.com>
2022-10-11 21:20:48 +0200

    interactive network

--- deep.py
+++ deep.py
@@ -1,7 +1,10 @@
+from re import T
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
+import networkx as nx
+from pyvis.network import Network
 
 # convert csv to dataframe
 
@@ -31,6 +34,13 @@ def mood(df):
             happiness.append(i['mood'])
             tags.append(i['emotions'])
 
+        # get all different happiness values
+        tag_values = []
+        for i in tags:
+            for j in i:
+                if j not in tag_values:
+                    tag_values.append(j)
+
         for d, h, t in zip(date, happiness, tags):
             # locate row with matching date
             date = df.loc[df['Date'] == d]
@@ -38,8 +48,13 @@ def mood(df):
             i = date.index[0]
             # insert happiness and tags into dataframe at index
             df.at[i, 'Mood'] = h
-            # df.at[i, 'Tags'] = t
-
+            for v in tag_values:
+                if v in t:
+                    df.at[i, v] = 1
+                else:
+                    df.at[i, v] = 0
+            
+            
 
 def convert(csv):
     # remove all empty columns from csv
@@ -47,7 +62,9 @@ def convert(csv):
     # convert date column to datetime
     df.Date = pd.to_datetime(df.Date)
     df.dropna(axis=1, how='all', inplace=True)
-    df.insert(47, "Sleep Delta (hr)", synthesize(df, "sleep_delta"))
+    # get index of column "Sleep Analysis [Asleep] (hr)"
+    i = df.columns.get_loc("Sleep Analysis [Asleep] (hr)")
+    df.insert(i, "Sleep Delta (hr)", synthesize(df, "sleep_delta"))
     mood(df)
     print("converted")
     return df
@@ -67,20 +84,65 @@ def correlation(df):
     plt.savefig('visualisations/correlation.png')
 
 
+def network(df, style):
+    corr = df.corr()
+    # correlation network graph
+    # instantiate networkx graph
+    plt.figure(figsize=(50, 50))
+
+    if style == "static":
+        net = nx.Graph()
+
+        # add nodes = columns from dataframe
+        net.add_nodes_from(df.columns)
+        # add edges = correlation values
+        for i in range(len(corr.columns)):
+            for j in range(i):
+                if abs(corr.iloc[i, j]) > 0.5:
+                    net.add_edge(corr.columns[i], corr.columns[j])
+        pos = nx.spring_layout(net, k=1.5, iterations=50)
+        nx.draw(net, pos, with_labels=True, node_color='blue',
+                edge_color='skyblue', width=1, linewidths=1)
+        # save the figure to file
+        plt.savefig('visualisations/static_network.png')
+    elif style == "dynamic":
+        net = Network(height="900px", width="100%",
+                      bgcolor="#222222", font_color=True)
+        net.set_options(open("options/default.txt").read())
+        # net.show_buttons(filter_=['edges', 'nodes'])
+
+        # add nodes with size according to how many edges they have
+        # get how many edges each node has
+        net.add_nodes(corr.columns)
+        for i in range(len(corr.columns)):
+            for j in range(i):
+                if abs(corr.iloc[i, j]) > 0.45 or abs(corr.iloc[i, j]) < -0.45:
+                    # set the color of the edge according to the correlation value
+                    if corr.iloc[i, j] > 0:
+                        color = "red"
+                    else:
+                        color = "blue"
+                    net.add_edge(corr.columns[i], corr.columns[j], color=color,
+                                 title="Correlation: " + str(corr.iloc[i, j]))
+        # save the graph
+        net.save_graph("visualisations/dynamic_network.html")
+
+
 def pair(df):
-    g = sns.pairplot(df[['Sleep Delta (hr)', 'Blood Pressure [Systolic] (mmHg)']],
+    g = sns.pairplot(df[['Headphone Audio Exposure (dBASPL)', 'Mood']],
                      kind='kde',
-                     plot_kws=dict(fill=False, color='purple', linewidths=1),
-                     diag_kws=dict(fill=False, color='purple', linewidth=1))
+                     plot_kws=dict(fill=False, color='black', linewidths=1),
+                     diag_kws=dict(fill=False, color='black', linewidth=1))
 
     # add observation dots
     g.map_offdiag(sns.scatterplot, marker='.', color='black')
     g.fig.set_size_inches(50, 50)
-    plt.show()
+    plt.savefig("visualisations/pairplot.png")
 
 
 if __name__ == '__main__':
     df = convert('dataset/export.csv')
-    correlation(df)
+
+    # correlation(df)
     # pair(df)
-    # mood(df)
+    network(df, "static")
