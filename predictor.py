5ddf8626bf33e3073318a0e62779defacc86925c
valentinfrlch <valentin.froehlich@gmail.com>
2022-10-31 18:00:33 +0100

    lineplot function

--- predictor.py
+++ predictor.py
@@ -1,3 +1,4 @@
+from configparser import LegacyInterpolation
 from tkinter import font
 from deep import *
 import helpers as helpers
@@ -19,38 +20,37 @@ def decompose(df, datapoint, period=24):
     samples = len(df)
     print("samples: ", samples)
     res = seasonal_decompose(df[datapoint].values[-samples:], period=period)
-    
-    
+
     observed = res.observed
     trend = res.trend
     seasonal = res.seasonal
     residual = res.resid
-    
-    #plot the complete time series
-    fig, axs = plt.subplots(4, figsize=(16,8))
+
+    # plot the complete time series
+    fig, axs = plt.subplots(4, figsize=(16, 8))
     axs[0].set_title('OBSERVED', fontsize=16)
     axs[0].plot(observed)
     axs[0].grid()
-    
-    #plot the trend of the time series
+
+    # plot the trend of the time series
     axs[1].set_title('TREND', fontsize=16)
     axs[1].plot(trend)
     axs[1].grid()
-    
-    #plot the seasonality of the time series. Period=24 daily seasonality | Period=24*7 weekly seasonality.
+
+    # plot the seasonality of the time series. Period=24 daily seasonality | Period=24*7 weekly seasonality.
     axs[2].set_title('SEASONALITY', fontsize=16)
     axs[2].plot(seasonal)
     axs[2].grid()
-    
-    #plot the noise of the time series
+
+    # plot the noise of the time series
     axs[3].set_title('NOISE', fontsize=16)
     axs[3].plot(residual)
     axs[3].scatter(y=residual, x=range(len(residual)), alpha=0.5)
     axs[3].grid()
-    
+
     plt.show()
-    
-    
+
+
 def autocorrelation(df, datapoint):
     # autocorrelation plot
     plot_acf(df[datapoint].values, lags=50)
@@ -60,190 +60,157 @@ def autocorrelation(df, datapoint):
 def predict(df, datapoint, horizon=7, plot=False, smoothness=10):
     X = df.drop(datapoint, axis=1)
     y = df[datapoint]
-    
-    
-    X_model, X_test = X.iloc[:-horizon,:], X.iloc[-horizon:,:]
+
+    X_model, X_test = X.iloc[:-horizon, :], X.iloc[-horizon:, :]
     y_model, y_test = y.iloc[:-horizon], y.iloc[-horizon:]
-    
-    #create, train and do inference of the model
+
+    # create, train and do inference of the model
     model = LGBMRegressor()
     model.fit(X_model, y_model)
     predictions = model.predict(X_test)
-    
+
     # calculate MAPE between predictions and test data
     accuracy = 0
-    
-    
+
     # rolling average of predictions and y_test
     y_test = y_test.rolling(smoothness).mean()
     # predictions to dataframe
-    predictions = pd.DataFrame(predictions, index=y_test.index, columns=[datapoint])
+    predictions = pd.DataFrame(
+        predictions, index=y_test.index, columns=[datapoint])
     # rolling average of predictions
     predictions = predictions.rolling(smoothness).mean()
-    
 
-    #plot reality vs prediction for the last week of the dataset
+    # plot reality vs prediction for the last week of the dataset
     if plot:
-            # add font
-            font_dir = ["./assets/"]
-            font_files = font_manager.findSystemFonts(fontpaths=font_dir)
-            for font_file in font_files:
-                font_manager.fontManager.addfont(font_file)
-                
-                
-            plt.figure(figsize=(16,6), facecolor='#021631')
-            ax = plt.axes()
-            
-            plt.grid(color='#6E7A8B')
-            
-            for tick in ax.get_xticklabels():
-                tick.set_fontname("Product Sans")
-            for tick in ax.get_yticklabels():
-                tick.set_fontname("Product Sans")
-            
-            ax.set_facecolor('#021631')
-            
-            ax.spines['bottom'].set_color('#6E7A8B')
-            ax.spines['top'].set_color('#6E7A8B')
-            ax.spines['left'].set_color('#6E7A8B')
-            ax.spines['right'].set_color('#6E7A8B')
-            
-            ax.set_title(f'{name_reconstruct(datapoint)} Measurements vs deepHealth Model', fontsize=20, color='white', fontname='Product Sans')
-            # set axis tick color
-            ax.tick_params(axis='x', colors='#6E7A8B')
-            ax.tick_params(axis='y', colors='#6E7A8B')
-            
-            
-            #set text color
-            plt.rcParams['text.color'] = 'white'
-            
-            # set prediction index to same index as y_test
-            predictions.index = y_test.index    
-            
-            plt.plot(y_test, label='Measurements', color='#00E89D')
-            plt.plot(predictions, label='Prediction', color='#77B7EE')
-            
-            plt.savefig(f'./visualisations/training_plots/{name_reconstruct(datapoint)}.png')
-            # close the plot
-            # remove plt from memory
-            plt.close()
-            plt.clf()
-            
-            # add logo to plot
-            helpers.logo(f'./visualisations/training_plots/{name_reconstruct(datapoint)}.png')
-            
+        # add font
+        font_dir = ["./assets/"]
+        font_files = font_manager.findSystemFonts(fontpaths=font_dir)
+        for font_file in font_files:
+            font_manager.fontManager.addfont(font_file)
+
+        plt.figure(figsize=(16, 6), facecolor='#021631')
+        ax = plt.axes()
+
+        plt.grid(color='#6E7A8B')
+
+        for tick in ax.get_xticklabels():
+            tick.set_fontname("Product Sans")
+        for tick in ax.get_yticklabels():
+            tick.set_fontname("Product Sans")
+
+        ax.set_facecolor('#021631')
+
+        ax.spines['bottom'].set_color('#6E7A8B')
+        ax.spines['top'].set_color('#6E7A8B')
+        ax.spines['left'].set_color('#6E7A8B')
+        ax.spines['right'].set_color('#6E7A8B')
+
+        ax.set_title(f'{name_reconstruct(datapoint)} Measurements vs deepHealth Model',
+                     fontsize=20, color='white', fontname='Product Sans')
+        # set axis tick color
+        ax.tick_params(axis='x', colors='#6E7A8B')
+        ax.tick_params(axis='y', colors='#6E7A8B')
+
+        # set text color
+        plt.rcParams['text.color'] = 'white'
+
+        # set prediction index to same index as y_test
+        predictions.index = y_test.index
+
+        plt.plot(y_test, label='Measurements', color='#00E89D')
+        plt.plot(predictions, label='Prediction', color='#77B7EE')
+
+        plt.savefig(
+            f'./visualisations/training_plots/{name_reconstruct(datapoint)}.png')
+        # close the plot
+        # remove plt from memory
+        plt.close()
+        plt.clf()
+
+        # add logo to plot
+        helpers.logo(
+            f'./visualisations/training_plots/{name_reconstruct(datapoint)}.png')
+
     return model, accuracy
-    
+
+
 def importance(model, dp):
-    #create a dataframe with the variable importances of the model
+    # create a dataframe with the variable importances of the model
     df_importances = pd.DataFrame({
         'feature': model.feature_name_,
         'importance': model.feature_importances_
     }).sort_values(by='importance', ascending=False)
-    
-    #plot variable importances of the model
+
+    # plot variable importances of the model
     plt.title(f'Variable Importances - {dp}', fontsize=16)
     # only show top 10
-    sns.barplot(x='importance', y='feature', data=df_importances.iloc[:20,:], palette='Blues_d', orient='h')
+    sns.barplot(x='importance', y='feature',
+                data=df_importances.iloc[:20, :], palette='Blues_d', orient='h')
     # change size of plot
     plt.gcf().set_size_inches(15, 4)
     plt.show()
     plt.close()
     plt.clf()
-    
+
+
 def train(df, horizon=7, smoothness=10):
     # iterate through columns
     for dp in df.columns:
         # show progress
         print(f"Processing {dp}")
         try:
-            lambda dp:re.sub('[^A-Za-z0-9_]+', '', dp)
-            mode, accuracy = predict(df, dp, horizon=horizon, plot=True, smoothness=smoothness)
+            lambda dp: re.sub('[^A-Za-z0-9_]+', '', dp)
+            mode, accuracy = predict(
+                df, dp, horizon=horizon, plot=True, smoothness=smoothness)
         except ValueError as e:
             print(e)
             continue
 
+
 def preprocess(path):
     df = convert(path)
     df = df.select_dtypes(exclude=['object'])
-    df = df.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+    df = df.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x))
     df = df.set_index('Date')
     return df
 
 
 def predict_next(df, horizon=7, smoothness=10):
     # create a progress bar
-    bar = progressbar.ProgressBar(maxval=len(df.columns), widgets=['Initializing Model...', progressbar.Percentage(), " ", progressbar.Bar('█', '|')], term_width=200)
+    bar = progressbar.ProgressBar(maxval=len(df.columns), widgets=[
+                                  'Initializing Model...', progressbar.Percentage(), " ", progressbar.Bar('█', '|')], term_width=200)
     bar.start()
     for dp in df.columns:
-        try:
+        #try:
             bar.update(bar.currval + 1)
             # change the title of the progress bar
             bar.widgets[0] = f'Processing {name_reconstruct(dp, True, True)} '
             X = df.drop(dp, axis=1)
             # take last week of data to validate model
-            X_test = X.iloc[-horizon:,:]
-            
+            X_test = X.iloc[-horizon:, :]
+
             model, accuracy = predict(df, dp)
             Y = model.predict(X_test)
-            
-            
+
             past_data = df[dp]
-            
+
             Y = pd.DataFrame(Y, columns=[dp])
-            
+
             merged = pd.merge(past_data, Y, how='outer')
             # calculate rolling average
             merged = merged.rolling(window=smoothness).mean()
             past = merged[dp].iloc[:-horizon]
             future = merged[dp].iloc[-horizon:]
-            
-            
-            # styling of plot
-            plt.figure(figsize=(16,6), facecolor='#021631')
-            ax = plt.axes()
-            
-            font_dir = ["./assets/"]
-            font_files = font_manager.findSystemFonts(fontpaths=font_dir)
-            for font_file in font_files:
-                font_manager.fontManager.addfont(font_file)
-                
-            plt.title(f'{name_reconstruct(dp)} Prediction', fontsize=20, color='white')
-            
-            plt.grid(color='#6E7A8B')
-            
-            ax.set_facecolor('#021631')
-            
-            ax.spines['bottom'].set_color('#6E7A8B')
-            ax.spines['top'].set_color('#6E7A8B')
-            ax.spines['left'].set_color('#6E7A8B')
-            ax.spines['right'].set_color('#6E7A8B')
-            # set axis tick color
-            ax.tick_params(axis='x', colors='#6E7A8B')
-            ax.tick_params(axis='y', colors='#6E7A8B')
-            
-            #set text color
-            plt.rcParams['text.color'] = 'white'
-            plt.rcParams["font.family"] = "Product Sans"
+
             # plot the data
-            plt.plot(past, color='#00E89D')  # 488BE3
-            plt.plot(future, color='#77B7EE')  # 0078FF
-            # connect the last point of past data to the first point of future data
-            plt.plot([len(past)-1, len(past)], [past.iloc[-1], future.iloc[0]], color='#77B7EE')
-            plt.plot(past.index[-1], past.iloc[-1], 'o', color='#00E89D', markersize=8)
-            
-            plt.savefig(f'./visualisations/predictions/{name_reconstruct(dp)}.png')
-            # close the plot
-            plt.close()
-            plt.clf()
-            
-            # add logo with helper function
-            helpers.logo(f'./visualisations/predictions/{name_reconstruct(dp)}.png')
-        except Exception as e:
-            continue
+            lineplot(f"Prediction of {name_reconstruct(dp)}", [
+                     [past, "#00E89D"], [future, "#77B7EE"]])
+        # except Exception as e:
+        #     print(e)
+        #     continue
 
 
-def lineplot(data):
+def lineplot(title, dptitle="", data=[]):
     """_summary_: Produces a lineplot of given data.
 
     Args:
@@ -258,7 +225,7 @@ def lineplot(data):
     for font_file in font_files:
         font_manager.fontManager.addfont(font_file)
 
-    plt.title(f'{name_reconstruct(dp)} Prediction', fontsize=20, color='white')
+    plt.title(title)
 
     plt.grid(color='#6E7A8B')
 
@@ -272,35 +239,39 @@ def lineplot(data):
     ax.tick_params(axis='x', colors='#6E7A8B')
     ax.tick_params(axis='y', colors='#6E7A8B')
 
-    #set text color
+    # set text color
     plt.rcParams['text.color'] = 'white'
     plt.rcParams["font.family"] = "Product Sans"
-    # plot the data
-    plt.plot(data[0], color='#00E89D')  # 488BE3
-    plt.plot(future, color='#77B7EE')  # 0078FF
-    # connect the last point of past data to the first point of future data
-    plt.plot([len(data[0])-1, len(data[0])], [data[0].iloc[-1], future.iloc[0], color='#77B7EE')
-    plt.plot(data[0].index[-1], data[0].iloc[-1], 'o', color='#00E89D', markersize=8)
 
-    plt.savefig(f'./visualisations/predictions/{name_reconstruct(dp)}.png')
+    for dp in data:
+        # plot the data from data[0] with color data[1]
+        plt.plot(dp[0], color=dp[1])
+
+    # add logo with helper function
+    plt.plot(data[0][0].index[-1], data[0][0].iloc[-1],
+             'o', color='#00E89D', markersize=8)
+
+    plt.savefig(f'./visualisations/predictions/{name_reconstruct(dptitle)}.png')
     # close the plot
     plt.close()
     plt.clf()
-        
-        
+    helpers.logo(f'./visualisations/predictions/{name_reconstruct(dptitle)}.png')
+
+
 def name_reconstruct(name, equalize=False, bold=False):
     # add a space before every capital letter
     # metrics to ignore
-    metrics = ["dBASPL", "mmHg", "kmhr", "min", "ms", "count", "countmin", "kcal", "g", "kg"]
+    metrics = ["dBASPL", "mmHg", "kmhr", "min",
+               "ms", "count", "countmin", "kcal", "g", "kg"]
     metrics.sort(key=len, reverse=True)
-    
+
     for metric in metrics:
         if name.endswith(metric):
             name = name[:-len(metric)]
             break
     name = re.sub(r'(?<!^)(?=[A-Z])', ' ', name)
     name = name[0].upper() + name[1:]
-    
+
     if equalize:
         # if name is too long, shorten it and add "..."
         if len(name) > 30:
@@ -310,8 +281,8 @@ def name_reconstruct(name, equalize=False, bold=False):
     if bold:
         name = f"\033[1m{name}\033[0m"
     return name
-    
-    
+
+
 if __name__ == '__main__':
     accuracies = []
     df = preprocess("dataset/export.csv")
@@ -329,8 +300,7 @@ if __name__ == '__main__':
     # get mean of all accuracies
     mean_accuracy = np.mean([float(acc[1][:-1]) for acc in accuracies])
     print(f"Mean accuracy: {mean_accuracy}%") """
-    
-    
+
     predict_next(df, horizon=90, smoothness=7)
     # decompose(df, 'Mood')
     # train(df, horizon=90, smoothness=10)
