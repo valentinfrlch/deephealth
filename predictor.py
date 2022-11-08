83926e6f86cd29d1e30af4c0f6eebc22d3da7105
valentinfrlch <valentin.froehlich@gmail.com>
2022-11-08 20:10:17 +0100

    some interpolation fixes (but they suck)

--- predictor.py
+++ predictor.py
@@ -269,8 +269,8 @@ def lineplot(title, dptitle, data, consecutive=True):
             d[0] = d[0][~d[0].index.duplicated(keep='first')]
             d[0] = d[0].sort_index()
             d[0] = d[0].iloc[10:, :]
-            # d[0] = d[0].reindex(pd.date_range(d[0].index[0], d[0].index[-1], freq='1H'))
-            # d[0] = d[0].interpolate(method='polynomial', order=5)
+            d[0] = d[0].reindex(pd.date_range(d[0].index[0], d[0].index[-1], freq='1H'))
+            d[0] = d[0].interpolate(method='polynomial', order=5)
             plt.plot(d[0], color=d[1])
         except ValueError as e:
             continue
